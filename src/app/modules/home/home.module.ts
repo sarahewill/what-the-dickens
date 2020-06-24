7bd998a63e084fddf4545054d723b53051782533
sarahwilliams <sarahewilliams@outlook.com>
2020-06-24 14:51:09 -0700

    add content to home page

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppMaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, FlexLayoutModule, AppMaterialModule],
 })
 export class HomeModule {}
