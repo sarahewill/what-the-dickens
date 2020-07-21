e0d338c9e42d744487694f71c94aaa52e3601991
sarahwilliams <sarahewilliams@outlook.com>
2020-07-20 18:06:04 -0700

    add destination

--- src/app/modules/walk/walk.component.ts
+++ src/app/modules/walk/walk.component.ts
@@ -3,6 +3,16 @@ import { Component, OnInit } from '@angular/core';
 import { CharactersService } from '../../services/characters/characters.service';
 import { Character } from '../../services/characters/character';
 
+export interface Walk {
+  startLocation: Location;
+  endLocation: Location;
+  character: Character;
+}
+export interface Location {
+  lat: number;
+  lng: number;
+}
+
 @Component({
   selector: 'app-walk',
   templateUrl: './walk.component.html',
@@ -12,6 +22,8 @@ export class WalkComponent implements OnInit {
   constructor(private charactersService: CharactersService) {}
   zoom = 12;
   assignedCharacter: Character;
+  usersLocation: Location;
+  endLocation: Location;
   center: google.maps.LatLngLiteral;
   options: google.maps.MapOptions = {
     mapTypeId: 'roadmap',
@@ -22,7 +34,7 @@ export class WalkComponent implements OnInit {
     minZoom: 8,
   };
   markers = [];
-
+  walk: Walk;
   ngOnInit() {
     this.center = {
       lat: 51.512732,
@@ -36,26 +48,55 @@ export class WalkComponent implements OnInit {
 
   addMarker() {
     // todo: get users location instead of random London location based on center
-    this.markers.push({
-      position: {
-        lat: this.center.lat + ((Math.random() - 0.5) * 2) / 10,
-        lng: this.center.lng + ((Math.random() - 0.5) * 2) / 10,
+    this.markers.push(
+      {
+        position: this.getRandomLatLong(),
+        label: {
+          color: 'black',
+          text: 'Your Location',
+        },
+        title: 'Marker title ' + (this.markers.length + 1),
+        info: 'Marker info ' + (this.markers.length + 1),
+        options: {
+          animation: google.maps.Animation.DROP,
+        },
       },
-      label: {
-        color: 'black',
-        text: 'Your Location',
-      },
-      title: 'Marker title ' + (this.markers.length + 1),
-      info: 'Marker info ' + (this.markers.length + 1),
-      options: {
-        animation: google.maps.Animation.DROP,
-      },
-    });
+      {
+        position: this.getRandomLatLong(),
+        label: {
+          color: 'black',
+          text: 'Your Destination',
+        },
+        title: 'Marker title ' + (this.markers.length + 1),
+        info: 'Marker info ' + (this.markers.length + 1),
+        options: {
+          animation: google.maps.Animation.DROP,
+        },
+      }
+    );
+  }
+
+  getRandomLatLong(): Location {
+    const location = {
+      lat: this.center.lat + ((Math.random() - 0.5) * 1.5) / 10,
+      lng: this.center.lng + ((Math.random() - 0.5) * 1.5) / 10,
+    };
+    this.usersLocation = location;
+    return location;
   }
 
   getRandomCharacter(): void {
     this.charactersService.getCharacters().subscribe((characters) => {
       this.assignedCharacter = characters[Math.floor(Math.random() * characters.length)];
     });
+  }
+
+  startWalk() {
+    this.walk = {
+      startLocation: this.usersLocation,
+      endLocation: this.endLocation,
+      character: this.assignedCharacter,
+    };
+    console.log('add start walk service here', this.walk);
   }
 }
